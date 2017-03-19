aff4a7d771ad3e21c8b923b2961d04f0ff763e69
egoing <egoing@gmail.com>
2017-03-19 15:19:20 +0900

    first commit

---
+++ src/file/EgoFileObject.php
@@ -0,0 +1,12 @@
+<?php
+class EgoFileObject extends SplFileObject
+{
+    function getContents(){
+        $content = $this->fread($this->getSize());
+        $this->rewind();
+        return $content;
+    }
+}
+
+//$file = new MyFileObject('EgoFileObject.php');
+//echo $file->getContents();
